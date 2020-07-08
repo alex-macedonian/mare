4481953732b61af3c5cbc2559fb27441de40de2b
alex-macedonian <consiorp@gmail.com>
2020-07-08 06:41:05 +0300

    distribution check added

--- scripts/motd.sh
+++ scripts/motd.sh
@@ -2,7 +2,7 @@
 #
 # motd.sh - configures the Message of the Day
 #
-# Copyright (C) 2019 - 2020 Alexandre Popov <amocedonian@gmail.com>.
+# Copyright (C) 2019 - 2020 Alexandre Popov <consiorp@gmail.com>.
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -18,17 +18,13 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-#################################
-# 			FUNCTIONS			#
-#################################
+DISTRO=$(awk '{if (($3 ~ "Debian") || ($3 ~ "LMDE")) {print $3}}' /usr/share/mare/version.list)
 
 # Preparing to customize the message of the day
 preparation_for_set_motd() 
 {
 
-	local LOGIN=$(awk 'FNR == 91 {print}' /etc/pam.d/login)
-	
-	echo -e "${MESAGE}\n"
+	local LOGIN_FILE=$(awk 'FNR == 91 {print}' /etc/pam.d/login)
 
 	if [ -s /etc/motd ]; then
 		cat /dev/null > /etc/motd
@@ -41,78 +37,50 @@ preparation_for_set_motd()
 	# check priority of message output of the day
 	# if the message about the last user authentication is displayed before the message  
 	# of the day, prompt the user to fix this by editing the /etc/pam.d/login file
-	if [ "$LOGIN" = "session    optional   pam_lastlog.so" ]; then
-		echo "Now in the console, the message about the last user"
-		echo -e "authentication is displayed before the message of the day.\n"
-		edit_login
-	# otherwise display a message that the message of the day is displayed 
-	# before the message about the last user authentication
-	else
-		echo "Now in the console, the message of the day is displayed"
-		echo -e "before the message about the last user authentication.\n"
+	if [ "$LOGIN_FILE" = "session    optional   pam_lastlog.so" ]; then
+		sed -i '100i\# Prints the last login info upon successful login' /etc/pam.d/login
+		sed -i '101i\# (Replaces the "LASTLOG_ENAB" option from login.defs)' /etc/pam.d/login
+		sed -i '102i\session    optional   pam_lastlog.so' /etc/pam.d/login
+		sed -i '103i\ ' /etc/pam.d/login
+		sed -i '89,92d' /etc/pam.d/login
 	fi
 	
 }
-
-# Edit the /etc/pam.d/login file
-edit_login() 
-{
-	
-	local CHAR=0
-
-	while
-		echo -n "Display message of the day first? (y or n): "
-		read CHAR
-		[ "$CHAR" != "n" ]; do
-		if [ "$CHAR" = "y" ]; then
-			if [ -f /etc/pam.d/login ]; then
-				sed -i '100i\# Prints the last login info upon successful login' /etc/pam.d/login
-				sed -i '101i\# (Replaces the "LASTLOG_ENAB" option from login.defs)' /etc/pam.d/login
-				sed -i '102i\session    optional   pam_lastlog.so' /etc/pam.d/login
-				sed -i '103i\ ' /etc/pam.d/login
-				sed -i '89,92d' /etc/pam.d/login
-			fi
-			break
-		else
-			echo "`basename $0:` you have entered an invalid character"
-		fi
-	done
-	
-}
-
-###################### BEGIN ######################
 
 # Setting up a dynamic message of the day. This is necessary
 # so that your console, after authentication, displays a
 # greeting and useful information about your operating system.
 
-# preparing to customize the message of the day
-preparation_for_set_motd
+if [ -n "$DISTRO" ]; then
+	# preparing to customize the message of the day
+	preparation_for_set_motd
 
-if [ -f /etc/update-motd.d/00-welcome-header ]; then
-	chmod +x /etc/update-motd.d/00-welcome-header
-fi
-
-if [ -f /etc/update-motd.d/10-system-info ]; then
-	chmod +x /etc/update-motd.d/10-system-info
-fi
-
-if [ -f /etc/update-motd.d/20-connect-info ]; then
-	chmod +x /etc/update-motd.d/20-connect-info
-
-	if [ -x /sbin/iwlist ]; then
-		echo "The wireless-tools package is already installed on your system."
-	else
-		# check the status of network interfaces
-		/usr/lib/mare/stifaces.sh
-		apt-get -y install wireless-tools
+	if [ -f /etc/update-motd.d/00-welcome-header ]; then
+		chmod +x /etc/update-motd.d/00-welcome-header
 	fi
+
+	if [ -f /etc/update-motd.d/10-system-info ]; then
+		chmod +x /etc/update-motd.d/10-system-info
+	fi
+
+	if [ -f /etc/update-motd.d/20-connect-info ]; then
+		chmod +x /etc/update-motd.d/20-connect-info
+
+		if [ -x /sbin/iwlist ]; then
+			echo "The wireless-tools package is already installed on your system."
+		else
+			# check the status of network interfaces
+			/usr/lib/mare/stifaces.sh
+			apt-get -y install wireless-tools
+		fi
 	
-	if [ -x /sbin/iw ]; then
-		echo "The iw package is already installed on your system."
-	else
-		apt-get -y install iw
+		if [ -x /sbin/iw ]; then
+			echo "The iw package is already installed on your system."
+		else
+			apt-get -y install iw
+		fi
 	fi
+else
+	echo "`basename $0:` you are using a different distribution GNU/Linux"
+	exit 1
 fi
-
-###################### END ######################
