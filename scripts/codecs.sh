778f20aaf85f2feb0411b8ec41cac88814664c7f
alex-macedonian <consiorp@gmail.com>
2020-07-14 22:01:58 +0300

    code readability improved

--- scripts/codecs.sh
+++ scripts/codecs.sh
@@ -19,6 +19,51 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+edit_sources_list()
+{
+	local STRING_MMEDIA=$(sed -n '19,20p' /etc/apt/sources.list)
+	
+	# check contents of sources.list file
+	# if the necessary lines exist in it, then inform the user about it
+	if [ -n "$STRING_MMEDIA" ]; then
+		echo "The www.deb-multimedia.org website has already"
+		echo "been added to the /etc/apt/sources.list file."
+	# otherwise edit the sources.list file
+	else
+		sed -i '14a\ ' /etc/apt/sources.list
+		sed -i '15a\deb '${SITE_MMEDIA}' buster main non-free' /etc/apt/sources.list
+		sed -i '16a\# deb-src '${SITE_MMEDIA}' buster main non-free' /etc/apt/sources.list
+	fi
+}
+
+check_package()
+{
+	local SITE_MMEDIA="http://www.deb-multimedia.org"
+
+	# check if the package is installed
+	# if the package is installed, then inform the user about it
+	if [ -f /etc/apt/trusted.gpg.d/deb-multimedia-keyring.gpg ]; then
+		echo "The deb-multimedia-keyring package is already installed on your system."
+	else
+		# create a directory to store temporary files
+		mkdir -p /tmp/mare
+		# change current working directory to /tmp/dpkg
+		cd /tmp/mare
+		# download the deb-multimedia-keyring package to the current working directory
+		wget $SITE_MMEDIA/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
+		
+		# return to user home directory
+		cd
+		# install the deb-multimedia-keyring package
+		dpkg --install /tmp/mare/deb-multimedia-keyring_2016.8.1_all.deb
+
+		apt update
+		apt -y upgrade
+		install_additional_codecs
+		clean_system
+	fi
+}
+
 install_additional_codecs() 
 {
 	local SYS_ARCH=$(arch)
@@ -47,55 +92,30 @@ install_additional_codecs()
 	apt-get -y install $PACKAGE_LIST
 }
 
-main()
+clean_system()
 {
-	local DEBIAN=$(awk '$1 ~ /Debian/ {print $1}' /usr/share/mare/version.list)
-	local STRING_MMEDIA=$(sed -n '19,20p' /etc/apt/sources.list)
-	local SITE_MMEDIA="http://www.deb-multimedia.org"	
+	# clean up garbage after installing packages
+	echo "Cleaning the system from unnecessary files and directories."
+	rm -R /tmp/mare
+	apt-get -y clean > /dev/null
+	apt-get -y autoremove > /dev/null
+}
+
+check_distribution()
+{
+	local DEBIAN=$(awk '$1 ~ /Debian/ {print $1}' /usr/share/mare/version.list)	
 	
 	if [ -n "$DEBIAN" ]; then
-		# check contents of sources.list file
-		# if the necessary lines exist in it, then inform the user about it
-		if [ -n "$STRING_MMEDIA" ]; then
-			echo "The www.deb-multimedia.org website has already"
-			echo "been added to the /etc/apt/sources.list file."
-		# otherwise edit the sources.list file
-		else
-			sed -i '14a\ ' /etc/apt/sources.list
-			sed -i '15a\deb '${SITE_MMEDIA}' buster main non-free' /etc/apt/sources.list
-			sed -i '16a\# deb-src '${SITE_MMEDIA}' buster main non-free' /etc/apt/sources.list
-		fi
-
-		# check if the package is installed
-		# if the package is installed, then inform the user about it
-		if [ -f /etc/apt/trusted.gpg.d/deb-multimedia-keyring.gpg ]; then
-			echo "The deb-multimedia-keyring package is already installed on your system."
-		else
-			# create a directory to store temporary files
-			mkdir -p /tmp/mare
-			# change current working directory to /tmp/dpkg
-			cd /tmp/mare
-			# download the deb-multimedia-keyring package to the current working directory
-			wget $SITE_MMEDIA/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
-		
-			# return to user home directory
-			cd
-			# install the deb-multimedia-keyring package
-			dpkg --install /tmp/mare/deb-multimedia-keyring_2016.8.1_all.deb
-
-			apt update
-			apt -y upgrade
-			install_additional_codecs
-		
-			# clean up garbage after installing packages
-			echo "Cleaning the system from unnecessary files and directories."
-			rm -R /tmp/mare
-			apt-get -y clean > /dev/null
-			apt-get -y autoremove > /dev/null
-		fi
+		edit_sources_list
+		check_package
 	else
 		echo "mare: this script only works with Debian GNU/Linux"
 	fi
+}
+
+main()
+{
+	check_distribution
 }
 
 main
